47027157c6c750899f62ebc0e286fb1e735b6c90
(Ethan) Jin Song Cheng <echeng@ci.com>
2017-11-17 16:22:15 -0500

    add more file.

---
+++ project/variables/script.js
@@ -0,0 +1,37 @@
+//Lecture: variables 1
+console.log('Hello World')
+
+var firstName = 'John';
+var lastName = "Smith";
+console.log(firstName + ' ' + lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+//Lecture: variables 2
+var name = 'Johh';
+var age = 26;
+
+console.log(name+26); //here, javascript automatically conver number to string.
+
+var job, isMarried;
+console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?');
+
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
