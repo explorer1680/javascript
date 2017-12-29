5f0dd97f2518033de594908dbc971a302856413a
Ethan Cheng <cjs1234567@hotmail.com>
2017-12-28 21:55:06 -0500

    add comment.

--- es6/promise/promise.js
+++ es6/promise/promise.js
@@ -3,19 +3,28 @@ let myFirstPromise = new Promise((resolve, reject) => {
     // In this example, we use setTimeout(...) to simulate async code. 
     // In reality, you will probably be using something like XHR or an HTML5 API.
     setTimeout(function () {
-        if(Math.random() > 0.5){
+        const t = Math.random();
+        if (t > 0.5) {
             resolve("Success!"); // Yay! Everything went well!
-        }else{
+        } else if(t > 0.3){
             reject("Sorry, it is fail.");
+        } else{
+            // throw new Error("This is an Error");//if you throw the Error, it can't be catched by .catch()
+            reject(new Error("This is an Error"));
         }
-        
+
     }, 2000);
 });
 
-myFirstPromise.then((successMessage) => {
+myFirstPromise
+.then((successMessage) => {
     // successMessage is whatever we passed in the resolve(...) function above.
     // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
     console.log("Yay! " + successMessage);
-}).catch((error) => {
-    console.log(error);
+}, 
+(rejected) => { console.log("Fall! " + rejected) })//here, you define the second method in then(), it is same as define it in catch().
+.catch((error) => {
+    console.log(error);//
 });
+
+//Please see the above example on how to receive rejected message from then() or from catch().
