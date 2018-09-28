130b49b0ace6adc7711f31a0491a7e531988d7c3
echeng <cjs1234567@hotmail.com>
2018-09-28 15:00:46 -0400

    modify docs.

--- ts/class/class.ts
+++ ts/class/class.ts
@@ -1,8 +1,10 @@
 //class in ts is different from class in ES6, I think it based on ES6's class.
 
 class Car{
-    name: string;//you have to define property before you can use it in constructor.
     private speed: number;//you can use private to constain the access.
+    name: string;//you have to define property before you can use it in constructor.
+    //otherwise, you have to use following constructor:
+    // constructor(private name, speed){ //if you use this one, you have to remove the definition above, it can't be deplicate.
     constructor(name, speed: number){//you can use type or not.
         this.name = name;
         this.speed = speed || 0;//set to 0 if it is a invalid value
