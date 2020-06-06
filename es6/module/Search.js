5f6fb48cc2bbd362853945ef6b76c2b41aa037e5
Ethan Cheng <cjs1234567@hotmail.com>
2020-06-06 12:03:55 -0400

    update project

--- es6/module/Search.js
+++ es6/module/Search.js
@@ -2,3 +2,8 @@ export const str = 'I am a default string!!!';
 export const add = (a, b) => a + b;
 export const multiply = (a, b) => a * b;
  
+let sum = 4 + 5;
+export default sum; 
+// export default a = 1;
+
+// export sum = 2+3;
--- es6/module/module.js
+++ es6/module/module.js
@@ -1,8 +1,11 @@
 import {str, add, multiply} from './Search.js';
+import abc from './Search.js';
 
 console.log(str);
 
 var sum = add(3,3);
 console.log(sum);
+
+console.log(abc);
 
 console.log( `2 + 3 = ${add(2, 3)}`);
