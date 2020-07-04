390b4b48f5a71fd1c212eea3fb7a92b25eaf8b06
Ethan Cheng <>
2020-07-04 08:18:47 -0400

    add code for generators

---
+++ es6/generators/generators.js
@@ -0,0 +1,80 @@
+function* process() {
+    console.log('Start process 1');
+    console.log('Pause process2 until call next()');
+
+    yield;
+
+    console.log('Resumed process2');
+    console.log('Pause process3 until call next()');
+
+    let parms = yield {age: 12};
+    console.log("Passed by final process next(90): " + parms);
+
+    console.log('Resumed process3');
+    console.log('End of the process function');
+}
+
+let _process = process();
+
+
+let value0 = _process.next(100);
+console.log(value0);
+let value1 = _process.next(100);
+console.log(value1);
+_process.next(60);
+
+
+
+function* one() {
+  while(true) {
+    var value = yield null;
+  }
+}
+var g1 = one(); 
+console.log(g1.next());
+console.log(g1.next(1000)); 
+console.log(g1.next(1000)); 
+
+//how yield works
+//1. it stop and return value. (even in the middle of a statement, it will stop)
+//like "let j = yield i", "yield i" will stop, then "let j = yield" will run after resume.
+//2. it resume and take parameter from next(), or it take parameter from next(), and resume.
+//please note, it take parameter from next(), and set to the variable. (only from second call to next())
+
+function* two() {
+  var i = 20;
+  while (true) {
+    // i = i + (yield i); 
+    i += yield i; //from second next(), the yield i will get parameter from next(), and calculate the i.
+  }
+}
+var g2 = two(); 
+console.log(g2.next(1000)); //first call not working
+console.log(g2.next(1000));
+console.log(g2.next(200));
+
+function* three(){
+  var index = 0;
+  while (true)
+    yield index++;
+}
+
+ var g3 = three();
+ console.log(g3.next());
+ console.log(g3.next(1000));
+ console.log(g3.next(1000));
+
+ function* gen() {
+    yield yield yield 5;
+  }
+
+var g4 = gen();
+console.log(g4.next(10));
+console.log(g4.next(10));
+console.log(g4.next(10));
+
+var g5 = gen();
+console.log(g5.next());
+console.log(g5.next());
+console.log(g5.next());
+
