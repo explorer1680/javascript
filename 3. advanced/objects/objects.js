cdff1bd46f7fc05e25c2d7e536f675e49ed96f63
Ethan Cheng <cjs1234567@hotmail.com>
2023-06-03 20:02:53 -0400

    update doc.

--- 3. advanced/objects/objects.js
+++ 3. advanced/objects/objects.js
@@ -1,22 +1,30 @@
 // Function constructor
 
-console.log(name);//where comes this name? It comes from the window object. 
-//Why window object has this property? I never define it.
-//Actually, in Note 1, there is a function constructor, inside of it, you have 'this.name=...' keyword, if you create Person instance with 'new' keyword, it is ok.
-//But, if you call it without 'new' keyword, like Note 2, the 'this' will point to window object, then the name is defined.
-//Please note, if you comment out the Note 2 and other place that use Person directly, you still have 'name' logged in console, you have to close this chrome window and open an new window, then it is disappeard.
+
+
+//2 observations here:
+//1. in following statement, name sometimes is '', sometimes has value
+console.log(name);
+//2. in following statement, it is an error
+//console.log(undefinedVar);
+//answer:
+//1. name vs undefinedVar:
+//It is because the 'name' property is predefined property in window object.
+//2. Actually, in Note 1, there is a function constructor, inside it, you have 'this.name=...', if you create Person instance with 'new' keyword, it is ok.
+//But, if you call it without 'new' keyword, like Note 2, the 'this' will point to window object, then the name property has value now.
+//Please note, sometime when you toggle the Person() function call, you still have 'name' with value logged in console, you have to close this chrome window and open an new window, then it is shown ''.
 //It is because the chrome window object don't refresh when you reload the page, the 'name' property just stay there even you don't have it in the program.
 
-//Another question, if you comment out the Note 2 and other place that use Person directly, and open a new tab, why the console.log(name) don't show like following:
-// console.log(undefinedVar);
-//It is because the 'name' property is predefined property in windows object.
+
 
 
 console.log(this);
 
 var john22  = {
     name : 'John222',
-    yearOfBirth: this.name,//this is weird, why sometimes it show 'John12' sometimes it show "", is this a bug for chrome?
+    //scenario 1: this.xxxxxx= 'xxxxxx', //you can't use this like inside of function.
+    yearOfBirth: this.name, //scenario 2: this point to window object.
+    //this is weird, why sometimes it show 'John12' sometimes it show "", is this a bug for chrome?
     //see the comment above.
     //when you define the object in this format, 'this' keyword in here is point to window object, 'this' keywork in method point to john22 object.
     abc: 'abc',
@@ -24,13 +32,13 @@ var john22  = {
     //yearOfBirth show empty string, it because the 'name' is predefined property in window object.
     w:this,//see, 'this' point to window object.
     job: 'teacher',
-    // this.xxxxxx= 'xxxxxx', //you can't use this like inside of function.
+    
     getInfo: function(){return '123'},
     calculateAge: function(param){
-        console.log(this);
+        console.log(this);  //scenario 3: this point to john22. //in method call, this point to object 
         console.log(this.yearOfBirth + ' ' + param);
         function oneMoreLayer(){
-            console.log(this);
+            console.log(this);// scenario 4: this point to window.
         }
         oneMoreLayer();
     }
@@ -44,28 +52,65 @@ john22.calculateAge('abc');
 
 //Capital letter means function constructor
 //Note 1.
-var Person = function(name, yearOfBirth, job){
+var MyPerson = function(name, yearOfBirth, job){
+    //scenario 1: this depending on how Person is used, if it is call be function, this is point to window, if it is called by new, it point to object.
     this.name = name;
-    xxxxxyyyyy = name;//without 'this', it will defined inside of windows object.
+    //scenario 2:
+    xxxxxyyyyy = name;//without 'this', it will defined inside of windows object, there is no difference between called by function or by called by new.
     this.yearOfBirth = yearOfBirth;
     this.job = job;
     this.calculateAge = function(){//in here, each instance will have this function(each one will have its own function), it is not inheritance.
-        console.log(2017-this.yearOfBirth);
+        console.log(this);
+        console.log(2017-this.yearOfBirth); ////scenario 3:  when you apply new, it create object, 'this' point to object; if you call it by function, 'this' point to window
         function showThis(){
-            console.log(this);
+            console.log(this);  //scenario 4: this is always point to window
         }
         showThis();
+    };
+    function aabbcc(){  //this definition is not an error, but calling aabbcc() is an error for both function call or new apply function call.
+        console.log(this);
+    };
+    ccddee = function(){ //this defintion is not an error, but calling ccddee() ppint to window by function call, with new apply function call, calling object.ccddee() is an error.
+        console.log(this);
     }
     this.create = function(){
         console.log('I am here');
     }
+    //we don't need to memorize the above scenarios, just follow the following "instantiation" comments.
+    //the function call will always create new EC object, for both normal function call and new apply function call, 
+    //the new apply function call will create new object, 'this' point to this new object
+    //by normal function, 'this' point to window.
+    //statement inside of the function is executed same, only the 'this' is different.
+    //why in scenario 3, this point to object, in scenario 4, this point to window?
+    //in scenario 3, it is method call, so, it this point to object.
+
+    // Function Execution Context
+    // In the Function Execution Context, how the this keyword is determined depends on how a function is called or invoked.
+
+    // A Javascript function can be invoked in four ways:
+
+    // Invocation as a function
+    // Invocation as a method
+    // Invocation as a constructor
+    // Invocation with the apply and call methods
        
     return '123';
     // return {a: 'a', b: 'b'};
     // return new Person2('HHH', 2000, 'BBB');//you can't use Person2 inside of Person. Maybe you can use it by move Person2 inside of Person.
     // return john22;
-
+    // looks like the return here only apply to the function call, not affect new Person(...)? return 123 not affact, return {a: 'a', b: 'b'} affect.
 }
+
+// var myperson = new MyPerson('my_name', 1999, 'teacher');
+// myperson.calculateAge();
+myPerson = new MyPerson('my_name', 1999, 'teacher');
+ccddee();
+myPerson.ccddee();
+
+Person('my_name', 1999, 'teacher');
+console.log(Person('my_name', 1999, 'teacher'));
+
+console.log(name);
 
 console.log(window);
 
@@ -76,7 +121,7 @@ console.log(window);
 //2. the constructor funciton is called. calling a function create a new Execution Context, 
 // in the normal function, 'this' variable point to the globle object,
 // but, in here, 'this' point to the brand new empty object created in step 1.
-// Here, you can see, the EC and object is not the same thing, but they are associated.
+// Here, you can see, the EC and object are not the same thing, but they are associated.
 //3. if the constructor function don't return anything, which is in this case, then the new object is returned.
 // item3 seems not correct, even it return '123', when using 'new', it return an object.
 // maybe, it because '123' is primitive.
@@ -158,12 +203,15 @@ var personProto = {
 
 }
 
+personProto.calculateAge();
+
 var john3 = Object.create(personProto);
 john3.name = 'John';
 john3.yearOfBirth = 1990;
 john3.job = 'teacher';
 
 console.log(john3);
+john3.calculateAge();
 
 var jane3 = Object.create(personProto,{//Please note, the syntax for define the object's properties: https://hackernoon.com/object-create-in-javascript-fa8674df6ed2
     name: { 
@@ -204,7 +252,7 @@ console.log(Object.create(null));
 
 console.log(Object.create(null,{a: {value: 'a'}}));
 
-var john1234 = (new Person('John1234', 1000, 'aaa')).create(); //what's this?
+var john1234 = (new Person('John1234', 1000, 'aaa')).create(); //what's this? new Person create an object, the create() is a method defined in Person(...)
 var p1 = new Person('John1234', 1000, 'aaa').create(); //new have high precedence, but . have higher precedence, this is same as following:
 console.log(p1);
 // var p11 = Person('John1234', 1000, 'aaa').create(); // this is not working
